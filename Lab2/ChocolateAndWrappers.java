6ee328a34dab1bfadefdcc9286cf591686345add
Daniela <39756160+dani0f@users.noreply.github.com>
2023-03-21 12:06:19 -0300

    Create ChocolateAndWrappers.java
    File with bugs

---
+++ Lab2/ChocolateAndWrappers.java
@@ -0,0 +1,23 @@
+    public static int countChoc(int chocolate, int wrap){
+        int newChocolate = chocolate / wrap;
+        if(newChocolate < 1){
+            return 0;
+        } else{
+          return countChoc(newChocolate, wrap);  
+        }
+    }
+    
+    public static int maxChoc(int money, int price, int wrap){
+        int chocolate = money / price;
+        int newChocolate = countChoc(chocolate, wrap);
+        return chocolate + newChocolate;
+    }
+    
+    
+    public static void main(String[] args) {
+        int money = 17;
+        int price = 3;
+        int wrap = 2;
+        int result = maxChoc(money,price,wrap);
+        System.out.println(result);
+    }
