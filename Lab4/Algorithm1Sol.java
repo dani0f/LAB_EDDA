872fb43959d188f9676b7b22c1a15ad0a9ace229
Daniela <39756160+dani0f@users.noreply.github.com>
2023-04-10 10:23:35 -0400

    Solutions

---
+++ Lab4/Algorithm1Sol.java
@@ -0,0 +1,17 @@
+public class Algorithm1Sol {
+    public static int LinearSearch(int key, int [] a){
+        for (int i = 0 ; i < a.length ; i ++){
+            if(a[i] == key) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 8, 4, 6};
+        long start = System.currentTimeMillis();
+        System.out.println(LinearSearch(6,arr  ));
+        long end = System.currentTimeMillis();
+        System.out.println("Time is " +  (end - start) + "ms" );
+    }
+
+}
---
+++ Lab4/Algorithm3Sol.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Algorithm3Sol {
+
+    public static int binarySearch(int key, int[] arr, int l, int r){
+        if( r >= l && l <= arr.length -1){
+            int mid = l + (r-l) / 2;
+            if(arr[mid] == key){
+                return mid;
+            }
+            if(arr[mid] > key){
+                return binarySearch(key, arr, l, mid-1);
+            }
+            return binarySearch(key, arr,mid+1, r);
+        }
+        return -1;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = {1, 2, 3, 8, 4, 6};
+        long start = System.currentTimeMillis();
+        System.out.println(binarySearch(2,arr, 0, arr.length -1));
+        long end = System.currentTimeMillis();
+        System.out.println("Time is " +  (end - start) + "ms" );
+    }
+
+}
